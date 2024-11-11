9c557497d210ddefc02a8805512ef3d8b78475d3
react-project <ajadhane1133@gmail.com>
2024-11-11 19:10:52 +0530

    done few changes

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function AboutMe() {
+  return (
+    <div className="container" id="aboutME">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>About Me</h2>
+          <p>
+            Front-End Developer with 3+ years of experience creating responsive,
+            interactive web applications using React.js,Next.js,JavaScript
+            (ES6+),HTML5, CSS3, Redux, React Router, and Material-UI. Skilled in
+            developing modular components that enhance user experiences and
+            optimize performance, scalability, and maintainability. Known for
+            efficient component lifecycle management, state handling, and clean
+            code practices that improve readability and project longevity.
+            Experienced in RESTful API integration, Git, Webpack, and Babel,
+            streamlining workflows and optimizing bundle sizes. Proficient in
+            responsive design with CSS preprocessors like SASS and frameworks
+            such as Bootstrap and TailwindCSS, ensuring accessibility and
+            cross-browser compatibility. In agile settings, my collaborative
+            approach, attention to detail, and problem-solving abilities drive
+            quality, deadline-oriented results. I’m passionate about delivering
+            seamless, user-centered applications.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
